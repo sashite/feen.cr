946951ebadef44dc68f8517e85e8f7bfdaf9704e
Cyril Kato <contact@cyril.email>
2020-09-15 23:24:54 +0200

    test(FEEN): add dump & parse methods

--- spec/feen_spec.cr
+++ spec/feen_spec.cr
@@ -1,47 +1,48 @@
 require "spec"
 require "../src/feen"
 
-# describe FEEN do
-#   describe ".dump" do
-#   end
+describe FEEN do
+  describe ".dump" do
+    it "returns the FEEN string representing the position" do
+      actual_value = FEEN.dump(
+        active_side_id: Int8.new(0),
+        board: {
+           3 => "s",
+           4 => "k",
+           5 => "s",
+          22 => "+P",
+          43 => "+B",
+        },
+        indexes: [Int8.new(9), Int8.new(9)],
+        pieces_in_hand_grouped_by_sides: [
+          %w[S],
+          %w[r r b g g g g s n n n n p p p p p p p p p p p p p p p p p],
+        ]
+      )
 
-#   describe ".parse" do
-#   end
+      actual_value.should eq("3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S/b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s")
+    end
+  end
 
-#   starting_position =
-#     FEEN.new(squares)
+  describe ".parse" do
+    it "returns the position params representing the position" do
+      actual_value = FEEN.parse("3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S/b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s")
 
-#   moves = [
-#     [56, 47, "P"],
-#     [3, 11, "g"],
-#     [64, 24, "+B", "P"],
-#     [5, 14, "g"],
-#     [24, 14, "+B", "G"],
-#     [4, 3, "k"],
-#     [nil, 13, "G"],
-#   ]
-
-#   last_position =
-#     moves.reduce(starting_position) do |position, move|
-#       position.call(move)
-#     end
-
-#   describe "#to_s" do
-#     it "describes the last position" do
-#       last_position.pieces_in_hand_grouped_by_sides.should eq([["P"], [] of String])
-
-#       last_position.squares.should eq(["l", "n", "s", "k", nil, nil, "s", "n", "l",
-#                                        nil, "r", "g", nil, "G", "+B", nil, "b", nil,
-#                                        "p", "p", "p", "p", "p", "p", nil, "p", "p",
-#                                        nil, nil, nil, nil, nil, nil, nil, nil, nil,
-#                                        nil, nil, nil, nil, nil, nil, nil, nil, nil,
-#                                        nil, nil, "P", nil, nil, nil, nil, nil, nil,
-#                                        "P", "P", nil, "P", "P", "P", "P", "P", "P",
-#                                        nil, nil, nil, nil, nil, nil, nil, "R", nil,
-#                                        "L", "N", "S", "G", "K", "G", "S", "N", "L"])
-
-#       last_position.in_hand_pieces.should eq([] of String)
-#       last_position.active_side_id.should eq(1)
-#     end
-#   end
-# end
+      actual_value.should eq({
+        active_side_id: 0,
+        board:          {
+           3 => "s",
+           4 => "k",
+           5 => "s",
+          22 => "+P",
+          43 => "+B",
+        },
+        indexes:                         [9, 9],
+        pieces_in_hand_grouped_by_sides: [
+          %w[S],
+          %w[b g g g g n n n n p p p p p p p p p p p p p p p p p r r s],
+        ],
+      })
+    end
+  end
+end
