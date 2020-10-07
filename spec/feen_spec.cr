cc9588f918ed2b76d266c8d8cf31761a89fddd3d
Cyril Kato <contact@cyril.email>
2020-10-07 21:00:36 +0200

    style: fix alignment

--- spec/feen_spec.cr
+++ spec/feen_spec.cr
@@ -27,15 +27,15 @@ describe FEEN do
 
       actual_value.should eq({
         side_id: 0,
-        square: {
+        square:  {
            3 => "s",
            4 => "k",
            5 => "s",
           22 => "+P",
           43 => "+B",
         },
-        shape: [9, 9],
-        in_hand: %w[S b g g g g n n n n p p p p p p p p p p p p p p p p p r r s]
+        shape:   [9, 9],
+        in_hand: %w[S b g g g g n n n n p p p p p p p p p p p p p p p p p r r s],
       })
     end
   end
